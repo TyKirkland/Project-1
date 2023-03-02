619e1ac4ba43608ee750401acd82f79c3583ee2a
Ty <Ty@Tys-Macbook.local>
2023-03-02 15:23:04 -0500

    second commit

---
+++ js/testscript.js
@@ -0,0 +1,242 @@
+let chess = {
+
+    properties: {
+        turn: 'w',
+        selectedpiece: '',
+        highlighted: [],
+        pieces: {
+            w_pawn1: {
+                position: '1_2',
+                img: '',
+                type: 'w_pawn',
+                captured: false,
+                moved: false
+            },
+            w_pawn2: {
+                position: '2_2',
+                img: '',
+                type: 'w_pawn',
+                captured: false,
+                moved: false
+            },
+            w_pawn3: {
+                position: '3_2',
+                img: '',
+                type: 'w_pawn',
+                captured: false,
+                moved: false
+            },
+            w_pawn4: {
+                position: '4_2',
+                img: '',
+                type: 'w_pawn',
+                captured: false,
+                moved: false
+            },
+            w_pawn5: {
+                position: '5_2',
+                img: '',
+                type: 'w_pawn',
+                captured: false,
+                moved: false
+            },
+            w_pawn6: {
+                position: '6_2',
+                img: '',
+                type: 'w_pawn',
+                captured: false,
+                moved: false
+            },
+            w_pawn7: {
+                position: '7_2',
+                img: '',
+                type: 'w_pawn',
+                captured: false,
+                moved: false
+            },
+            w_pawn8: {
+                position: '8_2',
+                img: '',
+                type: 'w_pawn',
+                captured: false,
+                moved: false
+            },
+            w_rook1: {
+                position: '1_1',
+                img: '',
+                type: 'w_rook',
+                captured: false,
+                moved: false
+            },
+            w_rook2: {
+                position: '8_1',
+                img: '',
+                type: 'w_rook',
+                captured: false,
+                moved: false
+            },
+            w_knight1: {
+                position: '2_1',
+                img: '',
+                type: 'w_knight',
+                captured: false,
+                moved: false
+            },
+            w_knight2: {
+                position: '7_1',
+                img: '',
+                type: 'w_knight',
+                captured: false,
+                moved: false
+            },
+            w_bishop1: {
+                position: '3_1',
+                img: '',
+                type: 'w_bishop',
+                captured: false,
+                moved: false
+            },
+            w_bishop2: {
+                position: '6_1',
+                img: '',
+                type: 'w_bishop',
+                captured: false,
+                moved: false
+            },
+            w_king: {
+                position: '5_1',
+                img: '',
+                type: 'w_king',
+                captured: false,
+                moved: false
+            },
+            w_queen: {
+                position: '4_1',
+                img: '',
+                type: 'w_queen',
+                captured: false,
+                moved: false
+            },
+            b_pawn1: {
+                position: '1_7',
+                img: '',
+                type: 'b_pawn',
+                captured: false,
+                moved: false
+            },
+            b_pawn2: {
+                position: '2_7',
+                img: '',
+                type: 'b_pawn',
+                captured: false,
+                moved: false
+            },
+            b_pawn3: {
+                position: '3_7',
+                img: '',
+                type: 'b_pawn',
+                captured: false,
+                moved: false
+            },
+            b_pawn4: {
+                position: '4_7',
+                img: '',
+                type: 'b_pawn',
+                captured: false,
+                moved: false
+            },
+            b_pawn5: {
+                position: '5_7',
+                img: '',
+                type: 'b_pawn',
+                captured: false,
+                moved: false
+            },
+            b_pawn6: {
+                position: '6_7',
+                img: '',
+                type: 'b_pawn',
+                captured: false,
+                moved: false
+            },
+            b_pawn7: {
+                position: '7_7',
+                img: '',
+                type: 'b_pawn',
+                captured: false,
+                moved: false
+            },
+            b_pawn8: {
+                position: '8_7',
+                img: '',
+                type: 'b_pawn',
+                captured: false,
+                moved: false
+            },
+            b_rook1: {
+                position: '1_8',
+                img: '',
+                type: 'b_rook',
+                captured: false,
+                moved: false
+            },
+            b_rook2: {
+                position: '8_8',
+                img: '',
+                type: 'b_rook',
+                captured: false,
+                moved: false
+            },
+            b_knight1: {
+                position: '2_8',
+                img: '',
+                type: 'b_knight',
+                captured: false,
+                moved: false
+            },
+            b_knight2: {
+                position: '7_8',
+                img: '',
+                type: 'b_knight',
+                captured: false,
+                moved: false
+            },
+            b_bishop1: {
+                position: '3_8',
+                img: '',
+                type: 'b_bishop',
+                captured: false,
+                moved: false
+            },
+            b_bishop2: {
+                position: '6_8',
+                img: '',
+                type: 'b_bishop',
+                captured: false,
+                moved: false
+            },
+            b_queen: {
+                position: '4_8',
+                img: '',
+                type: 'b_queen',
+                captured: false,
+                moved: false
+            },
+            b_king: {
+                position: '5_8',
+                img: '',
+                type: 'b_king',
+                captured: false,
+                moved: false
+            }
+        }
+    },
+    
+    methods: {
+        
+        gamesetup() {
+
+        },
+
+    }
+}
